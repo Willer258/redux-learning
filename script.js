1a9c31f8c03dcf348eddb2ba35d572b7a779d4c7
Wilfried <wilfriedhouinlindjonon91@gmail.com>
2021-09-17 19:27:55 +0000

    emmetre plsieurs actions

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 //ACTION
 
 const BUY_PHONE = "BUY_PHONE";
+const BUY_TABLET = "BUY_TABLET"
 
 function buyphone() {
   return {
-    type: BUY_PHONE,
+    type: BUY_PHONE 
   };
+}
+function buytablet(){
+    return {
+        type:BUY_TABLET
+    }
 }
 
 //Reducer
@@ -14,8 +20,8 @@ function buyphone() {
 //notre state initial
 const initialState = {
   phones: 10,
+  tablets: 20
 };
-
 //le reducer
 const reducer = (state = initialState, action) => {
   // l'action s'effectue grace au switch si le cas est bon alors le retour correspondant sera celui demande
@@ -26,6 +32,12 @@ const reducer = (state = initialState, action) => {
         ...state,
         phones: state.phones - 1,
       };
+      break;
+      case BUY_TABLET:
+      return {
+        ...state,
+        tablets: state.tablets - 1,
+      };
 
     default:
       return state;
@@ -35,10 +47,10 @@ const reducer = (state = initialState, action) => {
 //Redux.createStore(element)
 const store = Redux.createStore(reducer);
 
-const objetdispo = document.getElementById("count");
+const phonedispo = document.getElementById("count");
 
 //getState pour reruperer le state dans le reducer
-objetdispo.innerHTML = store.getState().phones;
+phonedispo.innerHTML = store.getState().phones;
 //dispatch pour emmetre l'action
 document.getElementById("buy-phone").addEventListener("click", function () {
   store.dispatch(buyphone());
@@ -46,5 +58,20 @@ document.getElementById("buy-phone").addEventListener("click", function () {
 
 //subscribe pour pouvoir recuprerer les etats du nouveau store
 store.subscribe(() => {
-  objetdispo.innerHTML = store.getState().phones;
+  phonedispo.innerHTML = store.getState().phones;
 });
+
+
+//Pour la tablette
+const tabletdispo = document.getElementById('count-tablet')
+tabletdispo.innerHTML = store.getState().tablets
+
+document.getElementById('buy-tablet').addEventListener("click",()=>{
+    store.dispatch(buytablet())
+})
+
+store.subscribe(()=>{
+    
+tabletdispo.innerHTML = store.getState().tablets
+
+})
