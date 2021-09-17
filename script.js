746e35ef613a1c5541e0bcbbb0d3d4471ae25fb4
Wilfried <wilfriedhouinlindjonon91@gmail.com>
2021-09-17 14:36:26 +0000

    creer un store

--- script.js
+++ script.js
@@ -8,27 +8,43 @@ function buyphone() {
   };
 }
 
-
 //Reducer
 // (prevState, action)=> newState
 
-
 //notre state initial
 const initialState = {
-    phones = 5
-}
+  phones: 10,
+};
 
 //le reducer
-const reducer = (state = initialState ,action)=>{
-// l'action s'effectue grace au switch si le cas est bon alors le retour correspondant sera celui demande
-   
-    switch (action.type) {
-        case BUY_PHONE:
-        return{
-            ...state,
-            phones:state.phones - 1
-        }
-        
-        default: return state
-    }
-}
+const reducer = (state = initialState, action) => {
+  // l'action s'effectue grace au switch si le cas est bon alors le retour correspondant sera celui demande
+
+  switch (action.type) {
+    case BUY_PHONE:
+      return {
+        ...state,
+        phones: state.phones - 1,
+      };
+
+    default:
+      return state;
+  }
+};
+
+//Redux.createStore(element)
+const store = Redux.createStore(reducer);
+
+const objetdispo = document.getElementById("count");
+
+//getState pour reruperer le state dans le reducer
+objetdispo.innerHTML = store.getState().phones;
+//dispatch pour emmetre l'action
+document.getElementById("buy-phone").addEventListener("click", function () {
+  store.dispatch(buyphone());
+});
+
+//subscribe pour pouvoir recuprerer les etats du nouveau store
+store.subscribe(() => {
+  objetdispo.innerHTML = store.getState().phones;
+});
